6ed58c515e9e3e6edfa48a1a1dcb9b3629adfc1d
TEV1996 <97004532+TEV1996@users.noreply.github.com>
2022-06-28 19:42:15 -0400

    Add files via upload

---
+++ LAb8AddCities.sql
@@ -0,0 +1,10 @@
+-- Insert name of database into the USE command below, save and execute
+--		all commands as a single batch.
+USE ;
+GO
+
+INSERT INTO Cities
+VALUES (1, 'Seattle', 520000),
+	   (2, 'Redmond', 110000),
+	   (3, 'Kirkland', 85000),
+	   (4, 'Bothell', 70000);
